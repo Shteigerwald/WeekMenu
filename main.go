1e02bebac8bcc968923255e78ff81a69a0a77009
Shtaiger <elemorta99@gmail.com>
2023-01-08 07:08:01 +0700

    Added:Добавил сущность jsonWeek хранящую структуру недели для формирования json вида Week. С помощью gin создал простое API, которая формирует наше меню по адресу localhost:8080/Menu

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 	"log"
 	"math/rand"
+	"net/http"
 	"os"
 	"time"
 )
@@ -23,36 +25,56 @@ type Day struct {
 	IdMeal sql.NullInt64
 }
 
-type week struct {
-	ID         int64
-	Days       [7]Day
-	Date_start string
+type Week struct {
+	ID        int64
+	Days      [7]Day
+	DateStart string
+}
+
+type jsonWeek struct {
+	Monday    string `json:"monday"`
+	Tuesday   string `json:"tuesday"`
+	Wednesday string `json:"wednesday"`
+	Thursday  string `json:"thursday"`
+	Friday    string `json:"friday"`
+	Saturday  string `json:"saturday"`
+	Sunday    string `json:"sunday"`
+	Date      string `json:"date"`
 }
 
 // SQL PART
 var db *sql.DB
 
-// init is invoked before main()
 func init() {
 
 	// loads values from .env into the system
 	if err := godotenv.Load(); err != nil {
 		log.Print("No .env file found")
 	}
-	// random
+	// random Seed From Time
 	rand.Seed(time.Now().UnixNano())
 }
 
 func main() {
+	//Router
+	router := gin.Default()
+	router.GET("/Menu", GetNextWeekMenu)
+	err := router.Run("localhost:8080")
+	if err != nil {
+		return
+	}
+}
+
+func GetNextWeekMenu(c *gin.Context) {
 	//Capture connection properties.
 	DBconnection()
 
 	//GET_LAST_WEEK
-	last_week, err := GetLastWeek()
+	lastWeek, err := GetLastWeek()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Information_about_LAST_week: %v\n ", last_week)
+	fmt.Printf("Information_about_LAST_week: %v\n ", lastWeek)
 
 	//GET_ALL_MEALS_FROM_DATABASE
 	AllMeals, err := GetAllMeals()
@@ -63,29 +85,47 @@ func main() {
 	for _, meal := range AllMeals {
 		fmt.Printf(" %v - %v, \n", meal.ID, meal.Title)
 	}
-
 	//GET_NEXT_WEEK
-	next_week, err := CreateNextWeek(last_week, AllMeals)
+	nextWeek, err := CreateNextWeek(lastWeek, AllMeals)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Print("\n Next_week: ", next_week)
+	fmt.Printf(" Next_week: %v\n", nextWeek)
 
-	//Add_NEXT_WEEK_TO_DB
-	Date, err := time.Parse("2006-01-02", last_week.Date_start)
-	if err != nil {
-		log.Fatal(err)
-	}
-	Date_Next_week := Date.AddDate(0, 0, 7)
-	var LastInsertId int64
-	LastInsertId, err = AddWeekToDB(next_week, Date_Next_week)
-	fmt.Printf("LastInsertID: %v\n", LastInsertId)
+	//var LastInsertId int64
+	//LastInsertId, err = AddWeekToDB(next_week, Date_Next_week)
+	//fmt.Printf("LastInsertID: %v\n", LastInsertId)
 
+	//Create_Json_week
+	var JsonWeekMenu jsonWeek
+	FillingJson(&JsonWeekMenu, nextWeek, AllMeals)
+	c.IndentedJSON(http.StatusOK, JsonWeekMenu)
 }
 
-func CreateNextWeek(lastweek week, allmeals []meal) (week, error) {
+func FillingJson(jsonWeek *jsonWeek, nextWeek Week, AllMeals []meal) {
+	jsonWeek.Monday = ApostleshipOf_MealID_and_MealTitle(nextWeek.Days[0].IdMeal.Int64, AllMeals)
+	jsonWeek.Tuesday = ApostleshipOf_MealID_and_MealTitle(nextWeek.Days[1].IdMeal.Int64, AllMeals)
+	jsonWeek.Wednesday = ApostleshipOf_MealID_and_MealTitle(nextWeek.Days[2].IdMeal.Int64, AllMeals)
+	jsonWeek.Thursday = ApostleshipOf_MealID_and_MealTitle(nextWeek.Days[3].IdMeal.Int64, AllMeals)
+	jsonWeek.Friday = ApostleshipOf_MealID_and_MealTitle(nextWeek.Days[4].IdMeal.Int64, AllMeals)
+	jsonWeek.Saturday = ApostleshipOf_MealID_and_MealTitle(nextWeek.Days[5].IdMeal.Int64, AllMeals)
+	jsonWeek.Sunday = ApostleshipOf_MealID_and_MealTitle(nextWeek.Days[6].IdMeal.Int64, AllMeals)
+	jsonWeek.Date = nextWeek.DateStart
+}
 
-	var nextweek week
+func ApostleshipOf_MealID_and_MealTitle(mealId int64, AllMeals []meal) (mealTitle string) {
+	for _, meal := range AllMeals {
+		if meal.ID == mealId {
+			return meal.Title
+		}
+	}
+	log.Fatal(fmt.Sprintf("ApostleshipOf_MealID_and_MealTitle doesn't have match SpecifiedID: %v and id in AllMeals", mealId))
+	return ""
+}
+
+func CreateNextWeek(lastweek Week, allmeals []meal) (Week, error) {
+
+	var nextweek Week
 
 	//PART: GetAllMealID from allmeals
 	var AllMealsID []int64
@@ -110,21 +150,21 @@ func CreateNextWeek(lastweek week, allmeals []meal) (week, error) {
 	fmt.Printf("LastWeekMealsID: %v\n", LastWeekMealsID)
 
 	//PART: Delete all index MealsIDLastWeak from AllMeals and create var Meals_ID_Without_Last_Week
-	Meals_ID_Without_Last_Week := AllMealsID
+	MealsIdWithoutLastWeek := AllMealsID
 	for _, lastweekmealID := range LastWeekMealsID {
 		// If we don't have meal this weak
 		if lastweekmealID == 0 {
 			continue
 		}
-		ZeroingElementFromSlice(&Meals_ID_Without_Last_Week, lastweekmealID-1)
+		ZeroingElementFromSlice(&MealsIdWithoutLastWeek, lastweekmealID-1)
 	}
-	fmt.Printf("Meals_ID_Without_Last_Week: %v, len: %v\n", Meals_ID_Without_Last_Week, len(Meals_ID_Without_Last_Week))
+	fmt.Printf("Meals_ID_Without_Last_Week: %v, len: %v\n", MealsIdWithoutLastWeek, len(MealsIdWithoutLastWeek))
 
 	//PART: Shuffle Meals_ID_Without_Last_Week
-	rand.Shuffle(len(Meals_ID_Without_Last_Week), func(i, j int) {
-		Meals_ID_Without_Last_Week[i], Meals_ID_Without_Last_Week[j] = Meals_ID_Without_Last_Week[j], Meals_ID_Without_Last_Week[i]
+	rand.Shuffle(len(MealsIdWithoutLastWeek), func(i, j int) {
+		MealsIdWithoutLastWeek[i], MealsIdWithoutLastWeek[j] = MealsIdWithoutLastWeek[j], MealsIdWithoutLastWeek[i]
 	})
-	fmt.Printf("Random Meals_ID_Without_Last_Week: %v\n", Meals_ID_Without_Last_Week)
+	fmt.Printf("Random Meals_ID_Without_Last_Week: %v\n", MealsIdWithoutLastWeek)
 
 	//PART: Filling the days for next week in var "nextweek"
 	var Days = [7]string{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
@@ -132,21 +172,21 @@ func CreateNextWeek(lastweek week, allmeals []meal) (week, error) {
 		var MealIndex int64 = 0
 		LenAllmeals := int64(len(allmeals))
 		fmt.Printf("!!!Day #%v\n", DayIndex)
-		fmt.Printf("Meals_ID_Without_Last_Week %v\n", Meals_ID_Without_Last_Week)
-		for Meals_ID_Without_Last_Week[MealIndex] == 0 {
+		fmt.Printf("Meals_ID_Without_Last_Week %v\n", MealsIdWithoutLastWeek)
+		for MealsIdWithoutLastWeek[MealIndex] == 0 {
 			MealIndex += 1
 			if MealIndex == LenAllmeals {
 				return nextweek, errors.New(fmt.Sprintf("BREAK: You don't have enough meals in database for rules, MealIndex = %v\n", LenAllmeals))
 			}
 		}
 		if DayIndex == 1 {
-			for allmeals[Meals_ID_Without_Last_Week[MealIndex]-1].main == allmeals[nextweek.Days[DayIndex-1].IdMeal.Int64-1].main ||
-				allmeals[Meals_ID_Without_Last_Week[MealIndex]-1].garnish == allmeals[nextweek.Days[DayIndex-1].IdMeal.Int64-1].garnish {
+			for allmeals[MealsIdWithoutLastWeek[MealIndex]-1].main == allmeals[nextweek.Days[DayIndex-1].IdMeal.Int64-1].main ||
+				allmeals[MealsIdWithoutLastWeek[MealIndex]-1].garnish == allmeals[nextweek.Days[DayIndex-1].IdMeal.Int64-1].garnish {
 				MealIndex += 1
 				if MealIndex == LenAllmeals {
 					return nextweek, errors.New(fmt.Sprintf("BREAK: You don't have enough meals in database for rules, MealIndex = %v\n", LenAllmeals))
 				}
-				for Meals_ID_Without_Last_Week[MealIndex] == 0 {
+				for MealsIdWithoutLastWeek[MealIndex] == 0 {
 					MealIndex += 1
 					if MealIndex == LenAllmeals {
 						return nextweek, errors.New(fmt.Sprintf("BREAK: You don't have enough meals in database for rules, MealIndex = %v\n", LenAllmeals))
@@ -154,20 +194,20 @@ func CreateNextWeek(lastweek week, allmeals []meal) (week, error) {
 				}
 			}
 			fmt.Printf("MEALINDEX: %v\n", MealIndex)
-			fmt.Printf("Meals_ID_Without_Last_Week[MealIndex]].main: %v\n", allmeals[Meals_ID_Without_Last_Week[MealIndex]-1].main)
+			fmt.Printf("Meals_ID_Without_Last_Week[MealIndex]].main: %v\n", allmeals[MealsIdWithoutLastWeek[MealIndex]-1].main)
 			fmt.Printf(" allmeals[nextweek.Days[DayIndex-1].IdMeal.Int64].main: %v\n", allmeals[nextweek.Days[DayIndex-1].IdMeal.Int64-1].main)
-			fmt.Printf("Meals_ID_Without_Last_Week[MealIndex]].garnish: %v\n", allmeals[Meals_ID_Without_Last_Week[MealIndex]-1].garnish)
+			fmt.Printf("Meals_ID_Without_Last_Week[MealIndex]].garnish: %v\n", allmeals[MealsIdWithoutLastWeek[MealIndex]-1].garnish)
 			fmt.Printf(" allmeals[nextweek.Days[DayIndex-1].IdMeal.Int64].garnish: %v\n", allmeals[nextweek.Days[DayIndex-1].IdMeal.Int64-1].garnish)
 		} else if DayIndex > 1 {
-			for allmeals[Meals_ID_Without_Last_Week[MealIndex]-1].main == allmeals[nextweek.Days[DayIndex-1].IdMeal.Int64-1].main ||
-				allmeals[Meals_ID_Without_Last_Week[MealIndex]-1].garnish == allmeals[nextweek.Days[DayIndex-1].IdMeal.Int64-1].garnish ||
-				allmeals[Meals_ID_Without_Last_Week[MealIndex]-1].main == allmeals[nextweek.Days[DayIndex-2].IdMeal.Int64-1].main ||
-				allmeals[Meals_ID_Without_Last_Week[MealIndex]-1].garnish == allmeals[nextweek.Days[DayIndex-2].IdMeal.Int64-1].garnish {
+			for allmeals[MealsIdWithoutLastWeek[MealIndex]-1].main == allmeals[nextweek.Days[DayIndex-1].IdMeal.Int64-1].main ||
+				allmeals[MealsIdWithoutLastWeek[MealIndex]-1].garnish == allmeals[nextweek.Days[DayIndex-1].IdMeal.Int64-1].garnish ||
+				allmeals[MealsIdWithoutLastWeek[MealIndex]-1].main == allmeals[nextweek.Days[DayIndex-2].IdMeal.Int64-1].main ||
+				allmeals[MealsIdWithoutLastWeek[MealIndex]-1].garnish == allmeals[nextweek.Days[DayIndex-2].IdMeal.Int64-1].garnish {
 				MealIndex += 1
 				if MealIndex == LenAllmeals {
 					return nextweek, errors.New(fmt.Sprintf("BREAK: You don't have enough meals in database for rules, MealIndex = %v\n", LenAllmeals))
 				}
-				for Meals_ID_Without_Last_Week[MealIndex] == 0 {
+				for MealsIdWithoutLastWeek[MealIndex] == 0 {
 					MealIndex += 1
 					if MealIndex == LenAllmeals {
 						return nextweek, errors.New(fmt.Sprintf("BREAK: You don't have enough meals in database for rules, MealIndex = %v\n", LenAllmeals))
@@ -175,31 +215,44 @@ func CreateNextWeek(lastweek week, allmeals []meal) (week, error) {
 				}
 			}
 			fmt.Printf("MEALINDEX: %v\n", MealIndex)
-			fmt.Printf("Meals_ID_Without_Last_Week[MealIndex]].main: %v\n", allmeals[Meals_ID_Without_Last_Week[MealIndex]-1].main)
+			fmt.Printf("Meals_ID_Without_Last_Week[MealIndex]].main: %v\n", allmeals[MealsIdWithoutLastWeek[MealIndex]-1].main)
 			fmt.Printf(" allmeals[nextweek.Days[DayIndex-1].IdMeal.Int64].main: %v\n", allmeals[nextweek.Days[DayIndex-1].IdMeal.Int64-1].main)
-			fmt.Printf("Meals_ID_Without_Last_Week[MealIndex]].garnish: %v\n", allmeals[Meals_ID_Without_Last_Week[MealIndex]-1].garnish)
+			fmt.Printf("Meals_ID_Without_Last_Week[MealIndex]].garnish: %v\n", allmeals[MealsIdWithoutLastWeek[MealIndex]-1].garnish)
 			fmt.Printf(" allmeals[nextweek.Days[DayIndex-1].IdMeal.Int64].garnish: %v\n", allmeals[nextweek.Days[DayIndex-1].IdMeal.Int64-1].garnish)
 		}
 		fmt.Printf("NextWeek %v Name потребовалось %v попыток для нахождения блюда\n", Days[DayIndex], MealIndex)
-		nextweek.Days[DayIndex].IdMeal.Int64 = Meals_ID_Without_Last_Week[MealIndex]
+		nextweek.Days[DayIndex].IdMeal.Int64 = MealsIdWithoutLastWeek[MealIndex]
 		nextweek.Days[DayIndex].IdMeal.Valid = true
-		ZeroingElementFromSlice(&Meals_ID_Without_Last_Week, MealIndex)
+		ZeroingElementFromSlice(&MealsIdWithoutLastWeek, MealIndex)
 		fmt.Printf("NextWeek %vID: %v\n", Days[DayIndex], nextweek.Days[DayIndex].IdMeal.Int64)
 		fmt.Printf("NextWeek %vName: %v\n", Days[DayIndex], allmeals[nextweek.Days[DayIndex].IdMeal.Int64-1].Title)
+
+		// Add Date
+		//Add_NEXT_WEEK_TO_DB
+		Date, err := time.Parse("2006-01-02", lastweek.DateStart)
+		if err != nil {
+			log.Fatal(err)
+		}
+		DateNextWeek := Date.AddDate(0, 0, 7)
+		nextweek.DateStart = DateNextWeek.Format("2006-01-02")
 	}
 	return nextweek, nil
 }
 
-// albumsByArtist queries for albums that have the specified artist name.
 func GetAllMeals() ([]meal, error) {
 	// An albums slice to hold data from returned rows.
 	var meals []meal
 
 	rows, err := db.Query("SELECT * FROM Meals")
 	if err != nil {
-		return nil, fmt.Errorf("Meals error: %v", err)
+		return nil, fmt.Errorf("Meals error: %v\n", err)
 	}
-	defer rows.Close()
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+
+		}
+	}(rows)
 	// Loop through rows, using Scan to assign column data to struct fields.
 	for rows.Next() {
 		var meal meal
@@ -214,8 +267,8 @@ func GetAllMeals() ([]meal, error) {
 	return meals, nil
 }
 
-func GetLastWeek() (week, error) {
-	var lastweek week
+func GetLastWeek() (Week, error) {
+	var lastweek Week
 	row := db.QueryRow("SELECT * FROM Week WHERE id = (SELECT MAX(id) FROM Week)")
 	err := row.Scan(&lastweek.ID,
 		&lastweek.Days[0].IdMeal,
@@ -225,7 +278,7 @@ func GetLastWeek() (week, error) {
 		&lastweek.Days[4].IdMeal,
 		&lastweek.Days[5].IdMeal,
 		&lastweek.Days[6].IdMeal,
-		&lastweek.Date_start,
+		&lastweek.DateStart,
 	)
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -240,19 +293,19 @@ func ZeroingElementFromSlice(slice *[]int64, index int64) {
 	(*slice)[index] = 0
 }
 
-func AddWeekToDB(week week, Date_last_week time.Time) (int64, error) {
-	result, err := db.Exec("INSERT INTO Week (Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Date_start)"+
-		" VALUES (?, ?, ?, ?, ?, ?, ?, ?)", week.Days[0].IdMeal, week.Days[1].IdMeal, week.Days[2].IdMeal,
-		week.Days[3].IdMeal, week.Days[4].IdMeal, week.Days[5].IdMeal, week.Days[6].IdMeal, Date_last_week)
-	if err != nil {
-		return 0, fmt.Errorf("AddWeekToDB: %v", err)
-	}
-	id, err := result.LastInsertId()
-	if err != nil {
-		return 0, fmt.Errorf("AddWeekToDB: %v", err)
-	}
-	return id, nil
-}
+//func AddWeekToDB(week week, DateLastWeek time.Time) (int64, error) {
+//	result, err := db.Exec("INSERT INTO Week (Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, DateStart)"+
+//		" VALUES (?, ?, ?, ?, ?, ?, ?, ?)", week.Days[0].IdMeal, week.Days[1].IdMeal, week.Days[2].IdMeal,
+//		week.Days[3].IdMeal, week.Days[4].IdMeal, week.Days[5].IdMeal, week.Days[6].IdMeal, DateLastWeek)
+//	if err != nil {
+//		return 0, fmt.Errorf("AddWeekToDB: %v", err)
+//	}
+//	id, err := result.LastInsertId()
+//	if err != nil {
+//		return 0, fmt.Errorf("AddWeekToDB: %v", err)
+//	}
+//	return id, nil
+//}
 
 func DBconnection() {
 	MysqlConfig := mysql.Config{
